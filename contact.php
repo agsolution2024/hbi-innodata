ea3851cafc3e8360bb5dc70846d105b2644bbe0b
Paul John Villalobos <PaulJohn.Villalobos@robinsonsretail.com.ph>
2024-12-04 23:08:07 +0800

    commit email revision

--- contact.php
+++ contact.php
@@ -1,40 +1,34 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+  $receiving_email_address = 'your-email@example.com'; // Set the receiving email address
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  // Set up email data
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['fname'] . ' ' . $_POST['lname']; // Concatenate first and last name
+  $contact->from_number = $_POST['number'];  // Optional: store the contact number separately
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host = 'smtp.gmail.com';                 //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'systemdeveloper.paul@gmail.com';                     //SMTP username
-    $mail->Password   = '!@Passw0rd';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port = 587;                                //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  $contact->smtp = array(
+    'host' => 'smtp.gmail.com',
+    'username' => '', // Enter your SMTP username (email)
+    'password' => '', // Enter your SMTP password
+    'port' => '465'
+  );
 
+  // Add messages
+  $contact->add_message( $_POST['fname'] . ' ' . $_POST['lname'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-    //Recipients
-    $mail->setFrom('noreplybhiinnodata@.com', '');
-    $mail->addAddress('systemdeveloper.paul@gmail.com', '');     //Add a recipient
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+  echo $contact->send();
+?>
