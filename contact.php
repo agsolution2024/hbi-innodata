0c62ee69a74b603c86739981691ce96341bfd187
Paul John Villalobos <PaulJohn.Villalobos@robinsonsretail.com.ph>
2024-12-04 23:13:47 +0800

    commit email changes

--- contact.php
+++ contact.php
@@ -1,32 +1,45 @@
 <?php
-  $receiving_email_address = 'your-email@example.com'; // Set the receiving email address
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'systemdeveloper.paul@gmail.com';
+
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    // include( $php_email_form );
+  // } else {
+    // die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  // Set up email data
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['fname'] . ' ' . $_POST['lname']; // Concatenate first and last name
-  $contact->from_number = $_POST['number'];  // Optional: store the contact number separately
+  $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => '', // Enter your SMTP username (email)
-    'password' => '', // Enter your SMTP password
-    'port' => '465'
-  );
+  // smtp.Port = 8889;
+  // smtp.Host = "mail5016.site4now.net"; //for gmail host  
+  // smtp.EnableSsl = false;
+  // // smtp.UseDefaultCredentials = false;
+  // smtp.Credentials = new NetworkCredential("mailer@ourladyofguadalupeschool.online", "!@Passw0rd");
 
-  // Add messages
-  $contact->add_message( $_POST['fname'] . ' ' . $_POST['lname'], 'From');
+
+  $contact->smtp = array(
+    'host' => 'mail5016.site4now.net',
+    'username' => 'mailer@ourladyofguadalupeschool.online',
+    'password' => '!@Passw0rd',
+    'port' => '8889'
+  );
+ 
+
+  $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
